b579cb117355ac192815e9d82002cf2d4d15d4a1
martnga <mtnjoroge@gmail.com>
2016-04-01 11:10:12 +0300

    Addede spinner

--- app/src/main/java/com/craft/PostaEbox/Fragments/Profile.java
+++ app/src/main/java/com/craft/PostaEbox/Fragments/Profile.java
@@ -1,12 +1,16 @@
 package com.craft.PostaEbox.Fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import com.craft.PostaEbox.GCMConfig.GCMRegistration;
 import com.craft.PostaEbox.R;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 /**
  * Created by mansa on 3/31/16.
@@ -18,6 +22,20 @@ public class Profile extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView=inflater.inflate(R.layout.fragment_profile, container, false);
+        MaterialBetterSpinner mPartnersSpinner = (MaterialBetterSpinner) rootView.findViewById(R.id.partners_spinner);
+        String[] list = new String[] {
+                "Belgium", "France", "Italy", "Germany", "Spain"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_dropdown_item_1line, list);
+
+        mPartnersSpinner.setAdapter(adapter);
+
+
+
+       /* b.putString("title", GCMRegistration.partnersMenuItems.get(position).get(KEY_PARTNER_NAME));
+        b.putString("partnerID", GCMRegistration.partnersMenuItems.get(position).get(KEY_PARTNER_ID));*/
         return rootView;
     }
 }
